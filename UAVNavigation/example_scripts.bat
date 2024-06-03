a593649b5e022fefea0f95629c4743bd937ba553
Sean Farrugia <seanf906@gmail.com>
2024-06-03 18:39:21 +0200

    scripts

--- UAVNavigation/example_scripts.bat
+++ UAVNavigation/example_scripts.bat
@@ -13,15 +13,15 @@ echo Tuning Cust (continuous) Random Multiple with PPO
 python tune_drone_algo_rllib.py -t cont --custom -a ppo -i 40 -p 4 -s 30 -b 4096 -w random_multiple --no-notif --max-steps 500
 
 echo Training Disc Random Single with PPO
-python train_drone_algo_rllib.py -t disc -a ppo -i 30 -b 4096 -w random_single --tuned
+python train_drone_algo_rllib.py -t disc -a ppo -i 30 -b 4096 -w random_single --tuned --max-steps 300
 
 echo Training Disc Random Multiple with PPO
-python train_drone_algo_rllib.py -t disc -a ppo -i 40 -b 4096 -w random_multiple --tuned
+python train_drone_algo_rllib.py -t disc -a ppo -i 40 -b 4096 -w random_multiple --tuned --max-steps 500
 
 echo Training Cont Random Single with PPO
-python train_drone_algo_rllib.py -t cont -a ppo -i 30 -b 4096 -w random_single --tuned
+python train_drone_algo_rllib.py -t cont -a ppo -i 30 -b 4096 -w random_single --tuned --max-steps 300
 
 echo Training Cont Random Multiple with PPO
-python train_drone_algo_rllib.py -t cont -a ppo -i 40 -b 4096 -w random_multiple --tuned
+python train_drone_algo_rllib.py -t cont -a ppo -i 40 -b 4096 -w random_multiple --tuned --max-steps 500
 
 echo Script execution completed.
