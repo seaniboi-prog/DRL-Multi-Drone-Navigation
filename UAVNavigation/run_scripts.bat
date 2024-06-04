a7e7b5febc13274109b777d975d85545b5e7fed5
Sean Farrugia <seanf906@gmail.com>
2024-06-04 22:08:01 +0200

    change script

--- UAVNavigation/run_scripts.bat
+++ UAVNavigation/run_scripts.bat
@@ -1,4 +1,4 @@
 @echo off
 
 echo Training Disc Random Multiple with PPO
-python train_drone_algo_rllib.py -t disc -a ppo -i 40 -b 2048 -w random_single --tuned --max-steps 400
+python train_drone_algo_rllib.py -t disc -a ppo -i 40 -b 2048 -w random_multiple --tuned --max-steps 800
