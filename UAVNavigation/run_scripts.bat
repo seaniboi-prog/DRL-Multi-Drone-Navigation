9d51d98da4d650a5605dccefdd431c55ab933d41
Sean Farrugia <seanf906@gmail.com>
2024-06-16 13:49:33 +0200

    Training Iteration 32 Finished using PPO in Cont Airsim with Random Multiple waypoints

--- UAVNavigation/run_scripts.bat
+++ UAVNavigation/run_scripts.bat
@@ -1,12 +1,5 @@
 @echo off
 
 echo Training Cont Random Multiple with PPO
-python train_drone_algo_rllib.py -t cont -a ppo -i 6 -b 4096 -w random_multiple --tuned --max-steps 400
-
-python restore_drone_algo_rllib.py -t cont -a ppo -i 6 -w random_multiple
-
-python restore_drone_algo_rllib.py -t cont -a ppo -i 6 -w random_multiple
-
-python restore_drone_algo_rllib.py -t cont -a ppo -i 6 -w random_multiple
 
 python restore_drone_algo_rllib.py -t cont -a ppo -i 6 -w random_multiple
