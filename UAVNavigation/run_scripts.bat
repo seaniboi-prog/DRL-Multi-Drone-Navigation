f1c951e1d785bcce153f53f36ee63ffc96f3e647
Sean Farrugia <seanf906@gmail.com>
2024-06-25 18:44:48 +0200

    tuning

--- UAVNavigation/run_scripts.bat
+++ UAVNavigation/run_scripts.bat
@@ -1,7 +1,7 @@
 @echo off
 
-echo Tuning Cust (discrete) Random Single with SAC
-python tune_drone_algo_rllib.py -t disc --custom -a sac -i 40 -p 4 -s 30 -b 4096 -w random_single --max-steps 400
+@REM echo Tuning Cust (discrete) Random Single with SAC
+@REM python tune_drone_algo_rllib.py -t disc --custom -a sac -i 40 -p 4 -s 30 -b 4096 -w random_single --max-steps 400
 
 echo Tuning Cust (discrete) Random Multiple with SAC
 python tune_drone_algo_rllib.py -t disc --custom -a sac -i 40 -p 4 -s 30 -b 4096 -w random_multiple --max-steps 500
