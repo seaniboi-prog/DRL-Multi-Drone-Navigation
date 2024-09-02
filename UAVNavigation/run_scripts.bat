d18341e4c4f9031cef7656d235038bf02b7b770a
Sean Farrugia <seanf906@gmail.com>
2024-09-02 23:12:00 +0200

    Training Iteration 32 Finished using MARWIL in Disc Airsim with Random Single waypoints

--- UAVNavigation/run_scripts.bat
+++ UAVNavigation/run_scripts.bat
@@ -1,25 +1,25 @@
 @echo off
 
 echo Training Disc Random Single with MARWIL
-python train_drone_algo_rllib.py -t disc -a marwil -i 40 -w random_single --tuned --max-steps 300
+python restore_drone_algo_rllib.py -t disc -a marwil -i 4 -w random_single
 
 echo Training Cont Random Single with MARWIL
-python train_drone_algo_rllib.py -t cont -a marwil -i 40 -w random_single --tuned --max-steps 300
+python train_drone_algo_rllib.py -t cont -a marwil -i 35 -w random_single --tuned --max-steps 300
 
 echo Training Disc Random Multiple with MARWIL
-python train_drone_algo_rllib.py -t disc -a marwil -i 40 -w random_multiple --tuned --max-steps 500
+python train_drone_algo_rllib.py -t disc -a marwil -i 35 -w random_multiple --tuned --max-steps 500
 
 echo Training Cont Random Multiple with MARWIL
-python train_drone_algo_rllib.py -t cont -a marwil -i 40 -w random_multiple --tuned --max-steps 500
+python train_drone_algo_rllib.py -t cont -a marwil -i 35 -w random_multiple --tuned --max-steps 500
 
-echo Training Disc Random Single with SAC
-python train_drone_algo_rllib.py -t disc -a sac -i 40 -w random_single --tuned --max-steps 300
+@REM echo Training Disc Random Single with SAC
+@REM python train_drone_algo_rllib.py -t disc -a sac -i 30 -w random_single --tuned --max-steps 300
 
-echo Training Cont Random Single with SAC
-python train_drone_algo_rllib.py -t cont -a sac -i 40 -w random_single --tuned --max-steps 300
+@REM echo Training Cont Random Single with SAC
+@REM python train_drone_algo_rllib.py -t cont -a sac -i 30 -w random_single --tuned --max-steps 300
 
-echo Training Disc Random Multiple with SAC
-python train_drone_algo_rllib.py -t disc -a sac -i 40 -w random_multiple --tuned --max-steps 500
+@REM echo Training Disc Random Multiple with SAC
+@REM python train_drone_algo_rllib.py -t disc -a sac -i 30 -w random_multiple --tuned --max-steps 500
 
-echo Training Cont Random Multiple with SAC
-python train_drone_algo_rllib.py -t cont -a sac -i 40 -w random_multiple --tuned --max-steps 500
+@REM echo Training Cont Random Multiple with SAC
+@REM python train_drone_algo_rllib.py -t cont -a sac -i 30 -w random_multiple --tuned --max-steps 500
