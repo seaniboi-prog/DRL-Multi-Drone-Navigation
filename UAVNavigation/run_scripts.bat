8f131d3e47c6130f9dcaec9621c445b74b3d73b3
Sean Farrugia <seanf906@gmail.com>
2024-09-10 20:01:37 +0200

    Single UAV Exp: multiple, PPO, Cont

--- UAVNavigation/run_scripts.bat
+++ UAVNavigation/run_scripts.bat
@@ -12,32 +12,34 @@
 @REM echo Training Cont Random Multiple with MARWIL
 @REM python train_drone_algo_rllib.py -t cont -a marwil -i 35 -w random_multiple --tuned --max-steps 500
 
-@REM echo Training Disc Random Single with SAC
-@REM python train_drone_algo_rllib.py -t disc -a sac -i 30 -w random_single --tuned --max-steps 300
 
-@REM echo Training Cont Random Single with SAC
-@REM python train_drone_algo_rllib.py -t cont -a sac -i 30 -w random_single --tuned --max-steps 300
+@REM echo Single UAV Experiment Single with PPO
+@REM python single_uav_experiment.py -a ppo -i 5 -w random_single -v single --best
 
-@REM echo Training Disc Random Multiple with SAC
-@REM python train_drone_algo_rllib.py -t disc -a sac -i 30 -w random_multiple --tuned --max-steps 500
+echo Single UAV Experiment Multiple with PPO
+python single_uav_experiment.py -a ppo -i 5 -w random_multiple -v multiple --best
 
-@REM echo Training Cont Random Multiple with SAC
-@REM python train_drone_algo_rllib.py -t cont -a sac -i 30 -w random_multiple --tuned --max-steps 500
+echo Single UAV Experiment Obstacle with PPO
+python single_uav_experiment.py -a ppo -i 5 -w random_multiple -v obstacle --best
 
-echo Single UAV Experiment Single with PPO Disc
-python single_uav_experiment.py -t disc -a ppo -i 10 -w random_single -v single --best
+echo Single UAV Experiment Single with MARWIL
+python single_uav_experiment.py -a marwil -i 5 -w random_single -v single --best
 
-echo Single UAV Experiment Single with PPO Cont
-python single_uav_experiment.py -t cont -a ppo -i 10 -w random_single -v single --best
+echo Single UAV Experiment Multiple with MARWIL
+python single_uav_experiment.py -a marwil -i 5 -w random_multiple -v multiple --best
 
-echo Single UAV Experiment Multiple with PPO Disc
-python single_uav_experiment.py -t disc -a ppo -i 10 -w random_multiple -v multiple --best
+echo Single UAV Experiment Obstacle with MARWIL
+python single_uav_experiment.py -a marwil -i 5 -w random_multiple -v obstacle --best
 
-echo Single UAV Experiment Multiple with PPO Cont
-python single_uav_experiment.py -t cont -a ppo -i 10 -w random_multiple -v multiple --best
+echo Training Disc Random Single with SAC
+python train_drone_algo_rllib.py -t disc -a sac -i 30 -w random_single --tuned --max-steps 300
 
-echo Single UAV Experiment Obstacle with PPO Disc
-python single_uav_experiment.py -t disc -a ppo -i 10 -w random_multiple -v obstacle --best
+echo Training Cont Random Single with SAC
+python train_drone_algo_rllib.py -t cont -a sac -i 30 -w random_single --tuned --max-steps 300
 
-echo Single UAV Experiment Obstacle with PPO Cont
-python single_uav_experiment.py -t cont -a ppo -i 10 -w random_multiple -v obstacle --best
+echo Training Disc Random Multiple with SAC
+python train_drone_algo_rllib.py -t disc -a sac -i 30 -w random_multiple --tuned --max-steps 500
+
+echo Training Cont Random Multiple with SAC
+python train_drone_algo_rllib.py -t cont -a sac -i 30 -w random_multiple --tuned --max-steps 500
+
