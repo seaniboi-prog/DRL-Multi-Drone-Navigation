73559caddf6f510a3f6414017245a5cbd8d7f111
Sean Farrugia <seanf906@gmail.com>
2024-09-12 21:01:07 +0200

    Training Iteration 20 Finished using SAC in Disc Airsim with Random Single waypoints

--- UAVNavigation/run_scripts.bat
+++ UAVNavigation/run_scripts.bat
@@ -35,8 +35,8 @@ echo Training Disc Random Single with SAC
 @REM python train_drone_algo_rllib.py -t disc -a sac -i 30 -w random_single --tuned --max-steps 300
 python restore_drone_algo_rllib.py -t disc -a sac -i 10 -w random_single
 
-echo Training Cont Random Single with SAC
-python train_drone_algo_rllib.py -t cont -a sac -i 30 -w random_single --tuned --max-steps 300
+@REM echo Training Cont Random Single with SAC
+@REM python train_drone_algo_rllib.py -t cont -a sac -i 30 -w random_single --tuned --max-steps 300
 
 @REM echo Training Disc Random Multiple with SAC
 @REM python train_drone_algo_rllib.py -t disc -a sac -i 30 -w random_multiple --tuned --max-steps 500
